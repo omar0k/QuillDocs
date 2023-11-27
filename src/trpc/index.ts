64bc30636d1111445273baed8668505b19b8c6af
omark01 <omark012000@gmail.com>
2023-11-27 00:18:21 -0500

    initial commit, finished landing page and trpc setup

---
+++ src/trpc/index.ts
@@ -0,0 +1,16 @@
+/*  */
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user?.id || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return { success: true };
+  }),
+});
+export type AppRouter = typeof appRouter;
