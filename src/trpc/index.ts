8947b8d8d389a820787feffa0d29b90454dda66f
omark01 <omark012000@gmail.com>
2023-11-27 19:27:12 -0500

    backend work, setting up prisma and finishing auth

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,14 +1,30 @@
 /*  */
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
     if (!user?.id || !user.email) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    //Check if user is in database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
     }
     return { success: true };
   }),
