15282d4cefe321a6d7983f17e200de218675112d
omark01 <omark012000@gmail.com>
2023-12-11 03:10:24 -0500

    stripe implementation to process payments

---
+++ src/app/dashboard/billing/page.tsx
@@ -0,0 +1,8 @@
+import BillingForm from "@/components/BillingForm";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+
+const Page = async () => {
+  const subscriptionPlan = await getUserSubscriptionPlan();
+  return <BillingForm subscriptionPlan={subscriptionPlan} />;
+};
+export default Page;
---
+++ src/config/stripe.ts
@@ -0,0 +1,22 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceIds: { test: "", production: "" },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 26,
+    price: {
+      amount: 14,
+      priceIds: { test: "price_1OM3UfCYg6awYEGrAgwNZoA8", production: "" },
+    },
+  },
+];
