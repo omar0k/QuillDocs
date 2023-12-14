db7effd8b329a92b6382b0506963d9dbc79cab36
omark01 <omark012000@gmail.com>
2023-12-14 02:39:36 -0500

    build error

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,5 +1,6 @@
 import { Pinecone } from "@pinecone-database/pinecone";
-export const PineconeClient = new Pinecone({
-  environment: "gcp-starter",
+
+export const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
+  environment: "gcp-starter",
 });
