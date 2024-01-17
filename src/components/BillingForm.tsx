27a344aba067945568a87f6ad9797ad73a8b1c63
omark01 <omark012000@gmail.com>
2024-01-17 11:17:03 -0500

    billing form updates/stripe fixes

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { FC } from "react";
 import { useToast } from "./ui/use-toast";
-import { trpc } from "@/app/_trpc/client";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import {
   Card,
@@ -14,33 +14,38 @@ import {
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 import { format } from "date-fns";
+import { trpc } from "@/app/_trpc/client";
 
 interface BillingFormProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>;
 }
 
-const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
+const BillingForm: FC<BillingFormProps> = ({ subscriptionPlan }) => {
   const { toast } = useToast();
+
   const { mutate: createStripeSession, isLoading } =
     trpc.createStripeSession.useMutation({
       onSuccess: ({ url }) => {
-        if (url) window.location.href = url;
+        if (url) {
+          window.location.href = url;
+        }
         if (!url) {
           toast({
             title: "There was a problem...",
-            description: "Please try again in a moment",
+            description: "Please try again later!",
             variant: "destructive",
           });
         }
       },
     });
-
+  console.log(subscriptionPlan.name);
   return (
     <MaxWidthWrapper className="max-w-5xl">
       <form
         className="mt-12"
         onSubmit={(e) => {
           e.preventDefault();
+          createStripeSession();
         }}
       >
         <Card>
@@ -51,12 +56,13 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
               plan.
             </CardDescription>
           </CardHeader>
-
           <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
             <Button type="submit">
               {isLoading ? (
                 <Loader2 className="mr-4 h-4 w-4 animate-spin" />
-              ) : null}
+              ) : (
+                <></>
+              )}
               {subscriptionPlan.isSubscribed
                 ? "Manage Subscription"
                 : "Upgrade to PRO"}
@@ -64,12 +70,19 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
             {subscriptionPlan.isSubscribed ? (
               <p className="rounded-full text-xs font-medium">
                 {subscriptionPlan.isCanceled
-                  ? "Your plan will be cancelled on"
-                  : "Your plan renew on"}
-                {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}
+                  ? "Your plan will be canceled on "
+                  : "Your plan renews on "}
+                <span className="underline underline-offset-2 text-sm">
+                  {format(
+                    subscriptionPlan.stripeCurrentPeriodEnd!,
+                    "dd.MM.yyyy"
+                  )}
+                </span>
                 .
               </p>
-            ) : null}
+            ) : (
+              <></>
+            )}
           </CardFooter>
         </Card>
       </form>
