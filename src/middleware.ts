61dc8d16ac1ede7ed935ba368002e309c0daf29c
omark01 <omark012000@gmail.com>
2024-01-17 12:41:23 -0500

    added middleware/redirects

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default function middleware(req:Request) {
+  return withAuth(req);
+}
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
